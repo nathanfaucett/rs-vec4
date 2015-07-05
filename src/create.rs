1f333be75978a3f12e651456fc3b68e63f5581a3
nathanfaucett <nathanfaucett@gmail.com>
2015-07-05 08:42:14 -0500

    updates

--- src/create.rs
+++ src/create.rs
@@ -13,23 +13,6 @@ fn test_create() {
 }
 
 #[inline(always)]
-pub fn zero<T: Num>() -> [T; 4] {create(T::zero(), T::zero(), T::zero(), T::zero())}
-#[inline(always)]
-pub fn identity<T: Num>() -> [T; 4] {create(T::zero(), T::zero(), T::zero(), T::one())}
-#[inline(always)]
-pub fn up<T: Num>() -> [T; 4] {create(T::zero(), T::zero(), T::one(), T::one())}
-#[inline(always)]
-pub fn down<T: Num>() -> [T; 4] {create(T::zero(), T::zero(), -T::one(), T::one())}
-#[inline(always)]
-pub fn forward<T: Num>() -> [T; 4] {create(T::zero(), T::one(), T::zero(), T::one())}
-#[inline(always)]
-pub fn back<T: Num>() -> [T; 4] {create(T::zero(), -T::one(), -T::zero(), T::one())}
-#[inline(always)]
-pub fn right<T: Num>() -> [T; 4] {create(T::one(), T::zero(), T::zero(), T::one())}
-#[inline(always)]
-pub fn left<T: Num>() -> [T; 4] {create(-T::one(), T::zero(), T::zero(), T::one())}
-
-#[inline(always)]
 pub fn clone<T: Num>(v: [T; 4]) -> [T; 4] {create(v[0], v[1], v[2], v[3])}
 
 #[inline(always)]
--- src/set.rs
+++ src/set.rs
@@ -15,3 +15,20 @@ fn test_set() {
     set(&mut v, 1, 2, 3, 4);
     assert!(v == [1, 2, 3, 4]);
 }
+
+#[inline(always)]
+pub fn zero<T: Num>(out: &mut [T; 4]) -> &mut [T; 4] { set(out, T::zero(), T::zero(), T::zero(), T::zero()) }
+#[inline(always)]
+pub fn identity<T: Num>(out: &mut [T; 4]) -> &mut [T; 4] { set(out, T::zero(), T::zero(), T::zero(), T::one()) }
+#[inline(always)]
+pub fn up<T: Num>(out: &mut [T; 4]) -> &mut [T; 4] { set(out, T::zero(), T::zero(), T::one(), T::one()) }
+#[inline(always)]
+pub fn down<T: Num>(out: &mut [T; 4]) -> &mut [T; 4] { set(out, T::zero(), T::zero(), -T::one(), T::one()) }
+#[inline(always)]
+pub fn forward<T: Num>(out: &mut [T; 4]) -> &mut [T; 4] { set(out, T::zero(), T::one(), T::one(), T::one()) }
+#[inline(always)]
+pub fn back<T: Num>(out: &mut [T; 4]) -> &mut [T; 4] { set(out, T::zero(), -T::one(), T::zero(), T::one()) }
+#[inline(always)]
+pub fn left<T: Num>(out: &mut [T; 4]) -> &mut [T; 4] { set(out, -T::one(), T::zero(), T::one(), T::one()) }
+#[inline(always)]
+pub fn right<T: Num>(out: &mut [T; 4]) -> &mut [T; 4] { set(out, T::one(), T::zero(), T::one(), T::one()) }
