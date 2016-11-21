fe416b3670bc12cfa4f43abb6a517aa84c35e4e8
nathanfaucett <nathanfaucett@gmail.com>
2016-11-21 09:57:07 -0600

    updates

--- src/create.rs
+++ src/create.rs
@@ -1,10 +1,10 @@
-use num::Num;
+use num::Unsigned;
 
 
 #[inline(always)]
-pub fn new<T: Num>(x: T, y: T, z: T, w: T) -> [T; 4] {[x, y, z, w]}
+pub fn new<T: Unsigned>(x: T, y: T, z: T, w: T) -> [T; 4] {[x, y, z, w]}
 #[inline(always)]
-pub fn create<T: Num>(x: T, y: T, z: T, w: T) -> [T; 4] {new(x, y, z, w)}
+pub fn create<T: Unsigned>(x: T, y: T, z: T, w: T) -> [T; 4] {new(x, y, z, w)}
 #[test]
 fn test_new() {
     let v = new(1, 2, 3, 4);
@@ -15,10 +15,10 @@ fn test_new() {
 }
 
 #[inline(always)]
-pub fn clone<'b, T: Num>(v: &'b [T; 4]) -> [T; 4] {new(v[0], v[1], v[2], v[3])}
+pub fn clone<'b, T: Unsigned>(v: &'b [T; 4]) -> [T; 4] {new(v[0], v[1], v[2], v[3])}
 
 #[inline(always)]
-pub fn copy<'a, 'b, T: Num>(out: &'a mut [T; 4], a: &'b [T; 4]) -> &'a mut [T; 4] {
+pub fn copy<'a, 'b, T: Unsigned>(out: &'a mut [T; 4], a: &'b [T; 4]) -> &'a mut [T; 4] {
     out[0] = a[0];
     out[1] = a[1];
     out[2] = a[2];
@@ -33,13 +33,13 @@ fn test_copy() {
 }
 
 #[inline(always)]
-pub fn from_vec2<'a, 'b, T: Num>(out: &'a mut [T; 4], v: &'b [T; 2]) -> &'a mut [T; 4] {
+pub fn from_vec2<'a, 'b, T: Unsigned>(out: &'a mut [T; 4], v: &'b [T; 2]) -> &'a mut [T; 4] {
     out[0] = v[0];
     out[1] = v[1];
     out
 }
 #[inline(always)]
-pub fn from_vec3<'a, 'b, T: Num>(out: &'a mut [T; 4], v: &'b [T; 3]) -> &'a mut [T; 4] {
+pub fn from_vec3<'a, 'b, T: Unsigned>(out: &'a mut [T; 4], v: &'b [T; 3]) -> &'a mut [T; 4] {
     out[0] = v[0];
     out[1] = v[1];
     out[2] = v[2];
--- src/misc.rs
+++ src/misc.rs
@@ -1,8 +1,8 @@
-use num::Num;
+use num::{Signed, Unsigned};
 
 
 #[inline(always)]
-pub fn inverse<'a, 'b, T: Num>(out: &'a mut [T; 4], a: &'b [T; 4]) -> &'a mut [T; 4] {
+pub fn inverse<'a, 'b, T: Signed>(out: &'a mut [T; 4], a: &'b [T; 4]) -> &'a mut [T; 4] {
     out[0] = -a[0];
     out[1] = -a[1];
     out[2] = -a[2];
@@ -20,7 +20,7 @@ fn test_inverse() {
 }
 
 #[inline(always)]
-pub fn lerp<'a, 'b, T: Num, N: Num>(out: &'a mut [T; 4], a: &'b [T; 4], b: &'b [T; 4], t: N) -> &'a mut [T; 4] {
+pub fn lerp<'a, 'b, T: Unsigned, N: Unsigned>(out: &'a mut [T; 4], a: &'b [T; 4], b: &'b [T; 4], t: N) -> &'a mut [T; 4] {
     let t_f64 = t.to_f64();
     out[0] = T::from_f64((a[0] + (b[0] - a[0])).to_f64() * t_f64);
     out[1] = T::from_f64((a[1] + (b[1] - a[1])).to_f64() * t_f64);
@@ -39,7 +39,7 @@ fn test_lerp() {
 }
 
 #[inline(always)]
-pub fn min<'a, 'b, T: Num>(out: &'a mut [T; 4], a: &'b [T; 4], b: &'b [T; 4]) -> &'a mut [T; 4] {
+pub fn min<'a, 'b, T: Unsigned>(out: &'a mut [T; 4], a: &'b [T; 4], b: &'b [T; 4]) -> &'a mut [T; 4] {
     out[0] = if b[0] < a[0] {b[0]} else {a[0]};
     out[1] = if b[1] < a[1] {b[1]} else {a[1]};
     out[2] = if b[2] < a[2] {b[2]} else {a[2]};
@@ -54,7 +54,7 @@ fn test_min() {
 }
 
 #[inline(always)]
-pub fn max<'a, 'b, T: Num>(out: &'a mut [T; 4], a: &'b [T; 4], b: &'b [T; 4]) -> &'a mut [T; 4] {
+pub fn max<'a, 'b, T: Unsigned>(out: &'a mut [T; 4], a: &'b [T; 4], b: &'b [T; 4]) -> &'a mut [T; 4] {
     out[0] = if b[0] > a[0] {b[0]} else {a[0]};
     out[1] = if b[1] > a[1] {b[1]} else {a[1]};
     out[2] = if b[2] > a[2] {b[2]} else {a[2]};
@@ -69,7 +69,7 @@ fn test_max() {
 }
 
 #[inline(always)]
-pub fn clamp<'a, 'b, T: Num>(out: &'a mut [T; 4], a: &'b [T; 4], min: &'b [T; 4], max: &'b [T; 4]) -> &'a mut [T; 4] {
+pub fn clamp<'a, 'b, T: Unsigned>(out: &'a mut [T; 4], a: &'b [T; 4], min: &'b [T; 4], max: &'b [T; 4]) -> &'a mut [T; 4] {
     out[0] = if a[0] < min[0] {min[0]} else if a[0] > max[0] {max[0]} else {a[0]};
     out[1] = if a[1] < min[1] {min[1]} else if a[1] > max[1] {max[1]} else {a[1]};
     out[2] = if a[2] < min[2] {min[2]} else if a[2] > max[2] {max[2]} else {a[2]};
@@ -81,4 +81,23 @@ fn test_clamp() {
     let mut v = [0, 0, 0, 0];
     clamp(&mut v, &[2, 2, 2, 2], &[0, 0, 0, 0], &[1, 1, 1, 1]);
     assert!(v == [1, 1, 1, 1]);
+}
+
+
+#[inline(always)]
+pub fn eq<'a, T: Unsigned>(a: &'a [T; 4], b: &'a [T; 4]) -> bool {
+    !nq(a, b)
+}
+
+#[inline(always)]
+pub fn nq<'a, T: Unsigned>(a: &'a [T; 4], b: &'a [T; 4]) -> bool {
+    !a[0].approx_eq(b[0]) ||
+    !a[1].approx_eq(b[1]) ||
+    !a[2].approx_eq(b[2]) ||
+    !a[3].approx_eq(b[3])
+}
+#[test]
+fn test_nq() {
+    assert_eq!(nq(&[1f32, 1f32, 1f32, 1f32], &[1f32, 1f32, 1f32, 1f32]), false);
+    assert_eq!(nq(&[0f32, 0f32, 0f32, 0f32], &[1f32, 1f32, 1f32, 1f32]), true);
 }
